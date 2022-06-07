7b332d859e27ea707461a56775041bafc0e28b0b
Nacho <44799327+nachom96@users.noreply.github.com>
2022-06-07 10:14:41 +0200

    Añadido modelo y variables en el provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+// En apuntes, está explicado cómo usar la Api.
 
 // Importar de material, no de cupertino
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '47b9d7290b6f057ee873786df73048b2';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('MoviesProvider inicializado');
 
@@ -9,6 +18,12 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final decodedData = json.decode(response.body) as Map<String, dynamic>;
+    print(decodedData['results']);
   }
 }
