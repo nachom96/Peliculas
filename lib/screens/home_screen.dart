e9ddf2c6b08674764ace31705f9a1a786c8d25bc
Nacho <44799327+nachom96@users.noreply.github.com>
2022-07-04 13:28:42 +0200

    Añadido SearchDelegate

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
 import 'package:peliculas/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
+  
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -11,11 +13,14 @@ class HomeScreen extends StatelessWidget {
     final moviesProvider = Provider.of<MoviesProvider>(context);
     return Scaffold(
         appBar: AppBar(
-          title: Text('Películas en cines'),
+          title: const Text('Películas en cines'),
           centerTitle: true,
           elevation: 0,
           actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined)),
+            IconButton(
+                onPressed: () =>
+                    showSearch(context: context, delegate: MovieSearchDelegate()),
+                icon: const Icon(Icons.search_outlined)),
           ],
         ),
         body: SingleChildScrollView(
---
+++ lib/search/search_delegate.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MovieSearchDelegate extends SearchDelegate{
+   
+   @override
+  String? get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      Text('buildActions')
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return Text('buildLeading');
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Text('buildSuggestions: $query');
+  }
+
+}
